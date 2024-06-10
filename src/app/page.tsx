7e3a39373b6cd770848204a31beae918e52c9b5d
sujalshrestha <sujalshresthawork@gmail.com>
2024-06-10 23:18:11 +0545

    users page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import Users from "@/components/users";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Users />
     </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ function HeroText() {
   return (
     <>
       <h1 className="mb-5 border-0 text-7xl">{heroText.title}</h1>
-      <p className="font-circularstdnormal mb-10 w-[62%] text-center text-lg text-slate-500">
+      <p className="mb-10 w-[62%] text-center font-circularstdnormal text-lg text-slate-500">
         {heroText.description}
       </p>
     </>
@@ -12,10 +12,10 @@ function HeroText() {
 }
 function Hero() {
   return (
-    <div className="font-circularstd bg-hero-primary flex flex-col items-center justify-center py-[90px]">
+    <div className="flex flex-col items-center justify-center bg-hero-primary pt-[90px] font-circularstd">
       <HeroText />
       <Image
-        className="border-hero-primary border-0"
+        className="border-0 border-hero-primary"
         src={"/hero.jpg"}
         alt={"hero"}
         width={840}
