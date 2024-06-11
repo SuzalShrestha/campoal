3d64e6854f0e78653ce43e5bdbde3196b4fdbd0b
sujalshrestha <sujalshresthawork@gmail.com>
2024-06-11 15:47:39 +0545

    numbers page

---
+++ src/components/data/numbers.tsx
@@ -0,0 +1,12 @@
+const numbers = [
+  { number: 20000, title: "Campaigns" },
+  {
+    number: 500000,
+    title: "Signatures",
+  },
+  {
+    number: 10000,
+    title: "Donations",
+  },
+];
+export default numbers;
